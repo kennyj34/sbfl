00d075f5ef5329038a6f54b0f18d06b518e57d4e
Kenny <joneskenneth34@gmail.com>
2023-01-07 21:33:23 -0500

    new commit

--- src/components/home/recordbook.js
+++ src/components/home/recordbook.js
@@ -11,30 +11,102 @@ const RecordBook = (props) => {
     color: "black",
     backgroundColor: "white",
     padding: "10px",
-    fontFamily: "Arial"
+    fontFamily: "Arial",
+    border:'1px solid black'
   };
   return (
 <div>
+  <h1>SBFL Record Book</h1>
 <table style={mystyle}>
-  <tr>
-    <th>Year</th>
-    <th>Champion</th>
-    <th>Runner-Up</th>
-    <th>Third Place</th>
-    <th>#1 Seed</th>
-    <th>#2 Seed</th>
-    <th>Sticker Bitch</th>
-    <th>Survivor</th>
+  <tr style={mystyle}>
+    <th style={mystyle}>Year</th>
+    <th style={mystyle}>Champion</th>
+    <th style={mystyle}>Runner-Up</th>
+    <th style={mystyle}>Third Place</th>
+    <th style={mystyle}>#1 Seed</th>
+    <th style={mystyle}>#2 Seed</th>
+    <th style={mystyle}>Sticker Bitch</th>
+    <th style={mystyle}>Survivor</th>
   </tr>
   <tr>
-    <td>Alfreds Futterkiste</td>
-    <td>Maria Anders</td>
-    <td>Germany</td>
+    <td style={{fontWeight:'bold'}}>2015</td>
+    <td style={mystyle}>Casey Conwell</td>
+    <td style={mystyle}>Frank Avenoso, Jr</td>
+    <td style={mystyle}>Matt Turowski</td>
+    <td style={mystyle}>Frank Avenoso, Jr</td>
+    <td style={mystyle}>Casey Conwell</td>
+    <td style={mystyle}>Drew Schuerholz</td>
+    <td style={mystyle}>Casey Conwell</td>
   </tr>
   <tr>
-    <td>Centro comercial Moctezuma</td>
-    <td>Francisco Chang</td>
-    <td>Mexico</td>
+    <td style={{fontWeight:'bold'}}>2016</td>
+    <td style={mystyle}>Anthony Cafiero</td>
+    <td style={mystyle}>Chris Kiernan</td>
+    <td style={mystyle}>John Snow</td>
+    <td style={mystyle}>Anthony Cafiero</td>
+    <td style={mystyle}>Chris Kiernan</td>
+    <td style={mystyle}>Dan Noonan</td>
+    <td style={mystyle}>Anthony Cafiero</td>
+  </tr>
+  <tr>
+    <td style={{fontWeight:'bold'}}>2017</td>
+    <td style={mystyle}>Anthony Cafiero</td>
+    <td style={mystyle}></td>
+    <td style={mystyle}></td>
+    <td style={mystyle}></td>
+    <td style={mystyle}></td>
+    <td style={mystyle}></td>
+    <td style={mystyle}></td>
+  </tr>
+  <tr>
+    <td style={{fontWeight:'bold'}}>2018</td>
+    <td style={mystyle}>Anthony Cafiero</td>
+    <td style={mystyle}></td>
+    <td style={mystyle}></td>
+    <td style={mystyle}></td>
+    <td style={mystyle}></td>
+    <td style={mystyle}></td>
+    <td style={mystyle}></td>
+  </tr>
+  <tr>
+    <td style={{fontWeight:'bold'}}>2019</td>
+    <td style={mystyle}>Anthony Cafiero</td>
+    <td style={mystyle}></td>
+    <td style={mystyle}></td>
+    <td style={mystyle}></td>
+    <td style={mystyle}></td>
+    <td style={mystyle}></td>
+    <td style={mystyle}></td>
+  </tr>
+  <tr>
+    <td style={{fontWeight:'bold'}}>2020</td>
+    <td style={mystyle}>Anthony Cafiero</td>
+    <td style={mystyle}></td>
+    <td style={mystyle}></td>
+    <td style={mystyle}></td>
+    <td style={mystyle}></td>
+    <td style={mystyle}></td>
+    <td style={mystyle}></td>
+  </tr>
+  <tr>
+    <td style={{fontWeight:'bold'}}>2021</td>
+    <td style={mystyle}>Anthony Cafiero</td>
+    <td style={mystyle}></td>
+    <td style={mystyle}></td>
+    <td style={mystyle}></td>
+    <td style={mystyle}></td>
+    <td style={mystyle}></td>
+    <td style={mystyle}></td>
+  </tr>
+  <tr>
+    <td style={{fontWeight:'bold'}}>2022</td>
+    <td style={mystyle}>Anthony Cafiero</td>
+    <td style={mystyle}></td>
+    <td style={mystyle}></td>
+    <td style={mystyle}></td>
+    <td style={mystyle}></td>
+    <td style={mystyle}></td>
+    <td style={mystyle}></td>
   </tr>
 </table>
 </div>
