f5b8d0fb7386ca9df82973b2365fc52801c3f796
kennyj34 <joneskenneth34@gmail.com>
2023-01-08 08:34:53 -0500

    commit

---
+++ src/components/home/home.js
@@ -0,0 +1,130 @@
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+
+export default function Home() {
+  return (
+  <div >
+    <h1 style={{display:'flex', justifyContent:'center'}}>Welcome to the SBFL Website</h1>
+    <div style={{display:'flex', justifyContent:'center'}}><img style={{mixBlendMode:'darken'}} width={250} height={250}
+              src='./asset/sbfl.jpeg' alt="image" /></div>
+    <p style={{display:'flex', justifyContent:'center', marginBottom:'100px'}}> 
+    SBFL is a prestigous fantasy football league created in 2015 from an Atlantic City hotel room. They employ the top of the line fantasy footballers in the world and Noonan
+    </p>
+    <div>
+      <h3>Belt Bowl Winners: </h3>
+    <Carousel variant='dark'>
+      <Carousel.Item>
+        <div style={{marginBottom:'100px'}}>
+        <div style={{display:'flex',justifyContent:'center'}}>
+          <h5>2022 Belt Bowl Winner: Casey Conwell</h5><br></br>
+          
+        </div>
+        <div style={{display:'flex',justifyContent:'center'}}>
+          <img style={{mixBlendMode:'darken'}}src='./asset/Snorks.jpg' alt="image" />
+        </div>
+        <Carousel.Caption>
+          <div style={{marginTop:'50px'}}>
+          Team: Butte Snorkelers
+          </div>
+        </Carousel.Caption>
+        </div>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div style={{marginBottom:'100px'}}>
+        <div style={{display:'flex', justifyContent:'center'}}>
+          <h5>2021 Belt Bowl Winner: Ira Dogruyol</h5>
+        </div>
+        <div style={{display:'flex',justifyContent:'center'}}>
+          Test
+        </div>
+        <Carousel.Caption>
+          <div>
+            Team: Carolina Reapers
+          </div>
+        </Carousel.Caption>
+        </div>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div style={{marginBottom:'100px'}}>
+        <div style={{display:'flex', justifyContent:'center'}}>
+          <h5>2020 Belt Bowl Winner: Matt Turowski</h5>
+        </div>
+        <div style={{display:'flex',justifyContent:'center'}}>
+          <img style={{mixBlendMode:'darken'}}src='./asset/Snorks.jpg' alt="image" />
+        </div>
+        <Carousel.Caption>
+          <div>
+            Team: NYC Pizza Rats
+          </div>
+        </Carousel.Caption>
+        </div>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div style={{marginBottom:'100px'}}>
+        <div style={{display:'flex', justifyContent:'center'}}>
+          <h5>2019 Belt Bowl Winner: Matt Turowski</h5>
+        </div>
+        <Carousel.Caption>
+          <div>
+            Team: NYC Pizza Rats
+          </div>
+        </Carousel.Caption>
+        </div>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div style={{marginBottom:'100px'}}>
+        <div style={{display:'flex', justifyContent:'center'}}>
+          <h5>2018 Belt Bowl Winner: John Snow</h5>
+        </div>
+        <Carousel.Caption>
+          <div>
+            Team: Kentucky Raccoons
+          </div>
+        </Carousel.Caption>
+        </div>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div style={{marginBottom:'100px'}}>
+        <div style={{display:'flex', justifyContent:'center'}}>
+          <h5>2017 Belt Bowl Winner: Casey Conwell</h5>
+        </div>
+        <Carousel.Caption>
+          <div>
+            Team: Butte Snorks
+          </div>
+        </Carousel.Caption>
+        </div>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div style={{marginBottom:'100px'}}>
+        <div style={{display:'flex', justifyContent:'center'}}>
+          <h5>2016 Belt Bowl Winner: Anthony Cafiero</h5>
+        </div>
+        <Carousel.Caption>
+          <div>
+            Team: Bagdhad Bombers
+          </div>
+        </Carousel.Caption>
+        </div>
+      </Carousel.Item>
+      <Carousel.Item>
+        <div style={{marginBottom:'100px'}}>
+        <div style={{display:'flex', justifyContent:'center'}}>
+          <h5>2015 Belt Bowl Winner: Casey Conwell</h5>
+        </div>
+        <Carousel.Caption>
+          <div>
+            Team: Butte Snorks
+          </div>
+        </Carousel.Caption>
+        </div>
+      </Carousel.Item>
+    </Carousel>
+    </div>
+    <div>
+      <h3>Podcast</h3>
+      <iframe style={{borderRadius:'12px'}} src="https://open.spotify.com/embed/show/1pkZSwHy4Gn7bI2KhOJs3a?utm_source=generator&theme=0" width="100%" height="200" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    </div>
+  </div>
+  );
+}
---
+++ src/components/home/recordbook.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+const RecordBook = (props) => {
+  //const images = props.images.map((image) => {
+  //return <img
+
+  //});
+  const mystyle = {
+    color: "black",
+    backgroundColor: "white",
+    padding: "10px",
+    fontFamily: "Arial"
+  };
+  return (
+<div>
+<table style={mystyle}>
+  <tr>
+    <th>Year</th>
+    <th>Champion</th>
+    <th>Runner-Up</th>
+    <th>Third Place</th>
+    <th>#1 Seed</th>
+    <th>#2 Seed</th>
+    <th>Sticker Bitch</th>
+    <th>Survivor</th>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+</table>
+</div>
+  );
+};
+
+export default RecordBook;
