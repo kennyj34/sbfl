35b3c8e4ae4197b6ab5c5c32a46d956d70983ac2
Kenny <joneskenneth34@gmail.com>
2023-01-26 14:33:52 -0500

    Correcting Home page

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -18,7 +18,7 @@ export default function Home() {
     <div style={{display:'flex', justifyContent:'center'}}><img style={{mixBlendMode:'darken'}} width={250} height={250}
               src='/asset/sbfl.jpg' alt="image" /></div>
     <p style={{display:'flex', justifyContent:'center', marginBottom:'100px'}}> 
-    SBFL is a prestigous fantasy football league created in 2015 from an Atlantic City hotel room. They employ the top of the line fantasy footballers in the world and Matt Cook
+    SBFL is a prestigous fantasy football league created in 2015 from an Atlantic City hotel room. They employ the top of the line fantasy footballers in the world and a very bad fantasy footballer, Matt Cook
     </p>
     </div>
     </Tab>
